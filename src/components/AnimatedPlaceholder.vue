26c47fd6de52f4ec7c0de11e7e6ff3de61201fe0
Chance Schafer <97748135+chance-schafer@users.noreply.github.com>
2023-03-31 01:28:27 -0500

    working on async view

---
+++ src/components/AnimatedPlaceholder.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="animated-placeholder">&nbsp;</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.animated-placeholder {
+  animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+  background-image: linear-gradient(to right, var(--tw-gradient-stops));
+  background-color: #f3f4f6;
+
+  @keyframes pulse {
+    0%,
+    100% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0.5;
+    }
+  }
+}
+</style>
---
+++ src/components/CityCardSkeleton.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="city-card-skeleton">
+    <div class="skeleton-one">
+      <AnimatedPlaceholder class="placeholder-one" />
+      <AnimatedPlaceholder class="placeholder-two" />
+    </div>
+
+    <div class="skeleton-two">
+      <AnimatedPlaceholder class="placeholder-three" />
+      <AnimatedPlaceholder class="placeholder-four" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AnimatedPlaceholder from "./AnimatedPlaceholder.vue";
+</script>
+
+<style lang="scss" scoped>
+.city-card-skeleton {
+  display: flex;
+  padding: 1.5rem 0.75rem;
+  border-radius: 0.375rem;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  background-color: $primary-color;
+
+  .skeleton-one {
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 0%;
+    gap: 0.5rem;
+
+    .placeholder-one {
+      max-width: 50%;
+    }
+
+    .placeholder-two {
+      max-width: 40%;
+    }
+  }
+
+  .skeleton-two {
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 0%;
+    align-items: flex-end;
+    gap: 0.5rem;
+
+    .placeholder-three {
+      width: 100%;
+      max-width: 50px;
+    }
+
+    .placeholder-four {
+      width: 100%;
+      max-width: 75px;
+    }
+  }
+}
+</style>
