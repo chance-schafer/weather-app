5fd629ccdec1a5e75c1ff6215d01f6528021b015
Chance Schafer <97748135+chance-schafer@users.noreply.github.com>
2023-04-06 14:48:36 -0500

    updated animated-placeholder

--- src/components/AnimatedPlaceholder.vue
+++ src/components/AnimatedPlaceholder.vue
@@ -7,6 +7,7 @@
   background-image: linear-gradient(to right, var(#fff 0%, #fff 100%));
   background-color: #f3f4f6;
   animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+  border-radius: 0.375rem;
 
   @keyframes pulse {
     0%,
